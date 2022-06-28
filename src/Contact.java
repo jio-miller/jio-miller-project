2f222e9aebef9248c54a6d74051a863f1cab797d
matthew jio <matthew.p.jio1@gmail.com>
2022-06-28 11:15:40 -0500

    first commit

---
+++ src/Contact.java
@@ -0,0 +1,11 @@
+public class Contact {
+    String name;
+    String number;
+
+    public Contact(String name, String number) {
+        this.name = name;
+        this.number = number;
+    }
+
+
+}
